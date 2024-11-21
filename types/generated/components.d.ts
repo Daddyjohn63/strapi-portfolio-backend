82b807b2ed62fca966937a25b64dd6f4b7d91445
Daddyjohn63 <john@webandprosper.co.uk>
2024-11-21 17:51:14 +0000

    added component

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,26 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface WebDesignProcessFlowWebDesignCard extends Schema.Component {
+  collectionName: 'components_web_design_process_flow_web_design_cards';
+  info: {
+    displayName: 'web design card';
+    icon: 'chartCircle';
+  };
+  attributes: {
+    Time: Attribute.String & Attribute.Required;
+    subheading: Attribute.String & Attribute.Required;
+    mainheading: Attribute.String & Attribute.Required;
+    description: Attribute.Text & Attribute.Required;
+    bullet_one: Attribute.String & Attribute.Required;
+    bullet_two: Attribute.String & Attribute.Required;
+    bullet_three: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'web-design-process-flow.web-design-card': WebDesignProcessFlowWebDesignCard;
+    }
+  }
 }
