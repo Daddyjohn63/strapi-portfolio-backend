f8361f669dbbce0c2c10a64a88cba5450fb1051b
Daddyjohn63 <john@webandprosper.co.uk>
2025-02-12 11:09:09 +0000

    removed trailing slash from mw

--- config/middlewares.js
+++ config/middlewares.js
@@ -11,7 +11,7 @@ module.exports = [
         process.env.NODE_ENV === "development"
           ? ["http://localhost:3000"]
           : [
-              "https://my-personal-website-mywebsite-nextjs.fbhmyq.easypanel.host/",
+              "https://my-personal-website-mywebsite-nextjs.fbhmyq.easypanel.host",
             ],
     },
   },
