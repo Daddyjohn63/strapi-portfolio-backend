a10e64945702fa8aeddf4038b40ef959e2f6b934
Daddyjohn63 <john@webandprosper.co.uk>
2025-02-12 10:54:21 +0000

    added cors to middelware

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,24 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      origin:
+        process.env.NODE_ENV === "development"
+          ? ["http://localhost:3000"]
+          : [
+              "https://my-personal-website-mywebsite-nextjs.fbhmyq.easypanel.host/",
+            ],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
